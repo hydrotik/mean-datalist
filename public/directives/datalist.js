2613fac684b9458f11cc985b1b8345bc03bf6e9f
Donovan Adams <donovan@hydrotik.com>
2015-03-01 14:48:57 -0500

    fixed dynamic directive issue

--- public/directives/datalist.js
+++ public/directives/datalist.js
@@ -9,7 +9,7 @@ angular.module('mean.datalist').directive('datalistfield', function(){
        template: '<div class="form-group">' + 
 	      '<label mean-token="create-{{item.id}}" class="col-md-3 control-label">Item {{item.label}}</label>' + 
 	      '<div class="col-md-9">' + 
-	        '<input name="{{item.id}}" type="text" class="form-control" data-ng-model="{{item.id}}" id={{item.id}}" placeholder="{{item.label}}" required>' + 
+	        '<input name="{{item.id}}" type="text" class="form-control" data-ng-model="item.id" id={{item.id}}" placeholder="{{item.label}}" required>' + 
 	        '</div>' + 
 	      '</div>' + 
 	    '</div>'
