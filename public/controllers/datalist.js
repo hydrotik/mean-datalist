311d51ee736186ebbe449b1181d52823a2778183
Donovan Adams <donovan@hydrotik.com>
2015-03-04 16:56:47 -0500

    date format on list

--- public/controllers/datalist.js
+++ public/controllers/datalist.js
@@ -9,82 +9,82 @@ angular.module('mean.datalist').controller('DataListController', [
     'DataList',
     'DataListModel',
   function($scope, $stateParams, $location, $timeout, Global, DataList, DataListModel) {
-    $scope.global = Global;
-    $scope.hasAuthorization = function(item) {
-      if (!item || !item.user) return false;
-      return $scope.global.isAdmin || item.user._id === $scope.global.user._id;
-    };
-    
-    $scope.schema = DataListModel.getFields();
+      $scope.global = Global;
+      $scope.hasAuthorization = function(item) {
+          if (!item || !item.user) return false;
+          return $scope.global.isAdmin || item.user._id === $scope.global.user._id;
+      };
+
+      $scope.schema = DataListModel.getFields();
 
 
-    $scope.item = {
-      _key : 'empty'
-    };
+      $scope.item = {
+          _key: 'empty'
+      };
 
-    DataListModel.clearFieldModels($scope.item);
+      DataListModel.clearFieldModels($scope.item);
 
-    $scope.create = function(isValid) {
-      var data = DataListModel.getData($scope.item);
+      $scope.create = function(isValid) {
+          var data = DataListModel.getData($scope.item);
 
-      if (isValid) {
-        var item = new DataList(data);
-        item.$save(function(response) {
-          $location.path('datalist/' + response._id);
-        });
+          if (isValid) {
+              var item = new DataList(data);
+              item.$save(function(response) {
+                  $location.path('datalist/' + response._id);
+              });
 
-        DataListModel.clearFieldModels($scope.item);
-      } else {
-        $scope.submitted = true;
-      }
-    };
-
-
-    $scope.remove = function(item) {
-      if (item) {
-        item.$remove(function(response) {
-          for (var i in $scope.items) {
-            if ($scope.items[i] === item) {
-	      $scope.items.splice(i,1);
-            }
+              DataListModel.clearFieldModels($scope.item);
+          } else {
+              $scope.submitted = true;
           }
-          $location.path('datalist');
-        });
-      } else {
-        $scope.item.$remove(function(response) {
-          $location.path('datalist');
-        });
-      }
-    };
+      };
 
-    $scope.update = function(isValid) {
-      if (isValid) {
-        var item = $scope.item;
-        if(!item.updated) {
-          item.updated = [];
-	}
-        item.updated.push(new Date().getTime());
 
-        item.$update(function() {
-          $location.path('datalist/' + item._id);
-        });
-      } else {
-        $scope.submitted = true;
-      }
-    };
+      $scope.remove = function(item) {
+          if (item) {
+              item.$remove(function(response) {
+                  for (var i in $scope.items) {
+                      if ($scope.items[i] === item) {
+                          $scope.items.splice(i, 1);
+                      }
+                  }
+                  $location.path('datalist');
+              });
+          } else {
+              $scope.item.$remove(function(response) {
+                  $location.path('datalist');
+              });
+          }
+      };
 
-    $scope.find = function() {
-      DataList.query(function(items) {
-        $scope.items = items;
-      });
-    };
+      $scope.update = function(isValid) {
+          if (isValid) {
+              var item = $scope.item;
+              if (!item.updated) {
+                  item.updated = [];
+              }
+              item.updated.push(new Date().getTime());
 
-    $scope.findOne = function() {
-      DataList.get({
-        itemid: $stateParams.itemid
-      }, function(item) {
-        $scope.item = item;
-      });
-    };
+              item.$update(function() {
+                  $location.path('datalist/' + item._id);
+              });
+          } else {
+              $scope.submitted = true;
+          }
+      };
+
+      $scope.find = function() {
+          DataList.query(function(items) {
+              $scope.items = items;
+          });
+      };
+
+      $scope.findOne = function() {
+          DataList.get({
+              itemid: $stateParams.itemid
+          }, function(item) {
+              $scope.item = item;
+          });
+      };
   }
 ]);
